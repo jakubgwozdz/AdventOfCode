9a94541cdbadeb71ebd1cfb4f10a0246a770a767
Jakub Gwozdz <jakub.gwozdz@gmail.com>
2019-12-02 17:45:25 +0100

    day 01

---
+++ src/advent2019/Day01.kt
@@ -0,0 +1,33 @@
+package advent2019
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun main() {
+    val sum = Files.readAllLines(Paths.get("input-2019-01.txt"))
+        .asSequence()
+        .map { it.toInt() }
+//        .map { it to fuel(it) } // first star
+        .map { it to fuel2(it) } // second star
+        .onEach { println(it) }
+        .map { it.second }
+        .sum()
+
+    println(sum)
+
+}
+
+fun fuel(mass: Int): Int {
+    return mass / 3 - 2
+}
+
+fun fuel2(mass: Int): Int {
+    var r = 0
+    var f = fuel(mass)
+    while (f > 0) {
+        r += f
+        f = fuel(f)
+    }
+    return r
+}
+
