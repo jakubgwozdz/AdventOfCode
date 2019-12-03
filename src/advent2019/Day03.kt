9fea3a5ca520077c41006b46fad272178ef07935
Jakub Gwozdz <jakub.gwozdz@gmail.com>
2019-12-03 17:46:42 +0100

    day 03 part 1 fixed

--- src/advent2019/Day03.kt
+++ src/advent2019/Day03.kt
@@ -6,60 +6,72 @@ import java.time.Instant
 import kotlin.math.absoluteValue
 
 fun main() {
-    val input = Files.readAllLines(Paths.get("input-2019-03.txt"))
-    part1(input.first(), input.drop(1).first())
+    val input = Files.readAllLines(Paths.get("input-2019-03.txt")).also {println("${Instant.now()}: data loaded")}
+    val first = input.first()
+    val second = input.drop(1).first()
+    calculate(first, second, ::metric2)
 }
 
-private fun wireToPlaces(wire: String): Set<Pair<Int, Int>> {
+typealias Place = Pair<Int, Int>
+data class Crossing(val place: Place, val wire1Distance: Int, val wire2Distance: Int)
+
+fun metric1(crossing: Crossing): Int {
+    return crossing.place.first.absoluteValue + crossing.place.second.absoluteValue
+}
+
+fun metric2(crossing: Crossing): Int {
+    return crossing.wire1Distance + crossing.wire2Distance
+}
+
+private fun movesForPart(part: String): List<Char> {
+    val direction = part[0]
+    val len = part.substring(1).toInt()
+    return (1..len).map { direction }
+}
+
+private fun positionsByPlaces(wire: String): Map<Place, Int> {
     return wire.split(',')
         .map { it.trim() }
-        .flatMap { part ->
-            val direction = part[0]
-            val len = part.substring(1).toInt()
-            (1..len).map { direction }
-        }
+        .flatMap { part -> movesForPart(part) }
         .also { println("${Instant.now()}: moves counted") }
-        .let {
-            var acc = 0 to 0
-            it.mapIndexed {i,c->
-                acc = when (c) {
-                    'U' -> acc.first to acc.second + 1
-                    'R' -> acc.first + 1 to acc.second
-                    'D' -> acc.first to acc.second - 1
-                    'L' -> acc.first - 1 to acc.second
-                    else -> error(c)
-                }
-                acc //to i
-            }
-        }
+        .let { placesAndPositions(it) }
         .also { println("${Instant.now()}: places counted: ${it.size}") }
-//        .distinctBy { it.first }
-        .toHashSet()
-        .also { println("${Instant.now()}: hashsetted :P") }
+        .distinctBy { it.first }
+        .toMap()
+        .also { println("${Instant.now()}: distinct places ${it.size}") }
+}
+
+private fun placesAndPositions(moves: List<Char>): List<Pair<Pair<Int, Int>, Int>> {
+    var acc = 0 to 0
+    return moves.mapIndexed { i, c ->
+        acc = when (c) {
+            'U' -> acc.first to acc.second + 1
+            'R' -> acc.first + 1 to acc.second
+            'D' -> acc.first to acc.second - 1
+            'L' -> acc.first - 1 to acc.second
+            else -> error(c)
+        }
+        acc to i + 1
+    }
 }
 
 
-fun findCrossings(wire1: String, wire2: String): Set<Pair<Int, Int>> {
+fun findCrossings(wire1: String, wire2: String): Collection<Crossing> {
 
-    val places1 = wireToPlaces(wire1)
-    val places2 = wireToPlaces(wire2)
+    val places1 = positionsByPlaces(wire1)
+    val places2 = positionsByPlaces(wire2)
 
-    val intersect = places1.intersect(places2).also { println("${Instant.now()}: intersections found: $it") }
+    val intersect = places1.keys.intersect(places2.keys).also { println("${Instant.now()}: intersections found: $it") }
 
-    return intersect
+    return intersect.map {
+        Crossing(it, places1[it]!!, places2[it]!!)
+    }
 }
 
-fun part1(wire1: String, wire2: String): Int {
+fun calculate(wire1: String, wire2: String, metric: (Crossing)->Int): Int {
     val intersections = findCrossings(wire1, wire2)
-    val distances = intersections.map { it.first.absoluteValue + it.second.absoluteValue }.also { println("${Instant.now()}: distances $it") }
-    val closest = distances.min()!!.also { println("${Instant.now()}: $it") }
+    val distances = intersections.map { metric(it) }.also { println("${Instant.now()}: distances $it") }
+    val closest = distances.min()!!.also { println("${Instant.now()}: result $it") }
     return closest
 }
-
-//fun part2(wire1: String, wire2: String): Int {
-//    val intersections = findCrossings(wire1, wire2)
-//    val distances = intersections.map { it.first.absoluteValue + it.second.absoluteValue }.also { println(it) }
-//    val closest = distances.min()!!.also { println("${Instant.now()}: $it") }
-//    return closest
-//}
 
--- testSrc/advent2019/Day03KtTest.kt
+++ testSrc/advent2019/Day03KtTest.kt
@@ -7,22 +7,56 @@ internal class Day03KtTest {
 
     @Test
     fun part1a() {
-        assertThat(part1("R8,U5,L5,D3", "U7,R6,D4,L4")).isEqualTo(6)
+        assertThat(calculate("R8,U5,L5,D3", "U7,R6,D4,L4", ::metric1)).isEqualTo(6)
     }
 
     @Test
     fun part1b() {
-        assertThat(part1("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83")).isEqualTo(159)
+        assertThat(
+            calculate(
+                "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+                "U62,R66,U55,R34,D71,R55,D58,R83",
+                ::metric1
+            )
+        ).isEqualTo(159)
     }
 
     @Test
     fun part1c() {
         assertThat(
-            part1(
+            calculate(
                 "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
-                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+                ::metric1
             )
         ).isEqualTo(135)
+    }
+
+    @Test
+    fun part2a() {
+        assertThat(calculate("R8,U5,L5,D3", "U7,R6,D4,L4", ::metric2)).isEqualTo(30)
+    }
+
+    @Test
+    fun part2b() {
+        assertThat(
+            calculate(
+                "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+                "U62,R66,U55,R34,D71,R55,D58,R83",
+                ::metric2
+            )
+        ).isEqualTo(610)
+    }
+
+    @Test
+    fun part2c() {
+        assertThat(
+            calculate(
+                "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+                ::metric2
+            )
+        ).isEqualTo(410)
     }
 
 }
