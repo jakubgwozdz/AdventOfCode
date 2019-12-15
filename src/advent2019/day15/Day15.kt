6bfd93bc5f70aaf991777da75966dd10f61649ed
Jakub Gwozdz <jakub.gwozdz@gmail.com>
2019-12-15 19:00:32 +0100

    day 15 finito

--- src/advent2019/day15/Day15.kt
+++ src/advent2019/day15/Day15.kt
@@ -18,22 +18,46 @@ fun main() {
     val program = readAllLines("input-2019-15.txt").single()
         .also { logWithTime("Program length (chars): ${it.length}") }
 
-    logWithTime("Part1: Blocks: ${part1(program)}")
+    val memory = parse(program)
+    val map = mapShip(memory)
+    val start: Location = 0 to 0
+    val end = map.filterValues { it == MapContent.Target }.keys.single()
+    logWithTime("$start - $end")
 
-//    logWithTime("Part2: Score: ${part2(program)}")
+    val part1 = shortestTo(end, map, mutableMapOf(start to emptyList()), emptyList())
+        .also { logWithTime("$it") }!!
+        .size
+    logWithTime("Part1: Distance: $part1")
+
+    // part2
+    val cache = mutableMapOf<Location, List<Location>>(end to emptyList())
+    val part2 = map.filter { (_, c) -> c.canGoThrough }
+        .keys
+        .map { shortestTo(it, map, cache, emptyList()) ?: error("cant get to $it") }
+        .map { it.size }
+        .max()!!
+
+    logWithTime("Part2: Time to fill: $part2")
 
 }
 
-fun part1(program: String): Int {
-    val memory = parse(program)
-
-    val map = mapShip(memory)
-    val start = 0 to 0
-    val end = map.filterValues { it == MapContent.Target }.keys.single()
-
-    logWithTime("$start - $end")
-
-    return 0
+fun shortestTo(
+    end: Location,
+    map: Map<Location, MapContent>,
+    cache: MutableMap<Location, List<Location>>,
+    visited: List<Location>
+): List<Location>? {
+    return Direction.values()
+        .map { end + it }
+        .filter { !visited.contains(it) }
+        .filter { map[it]?.canGoThrough ?: error("from $end to $it???") }
+        .mapNotNull { newEnd ->
+            cache[newEnd]
+                ?: shortestTo(newEnd, map, cache, visited + newEnd)
+                    ?.also { cache[newEnd] = it }
+        }
+        .minBy { it.size }
+        ?.let { it + end }
 }
 
 typealias Location = Pair<Int, Int>
@@ -55,7 +79,6 @@ enum class MapContent(val code: BigInteger, val canGoThrough: Boolean) {
 operator fun Location.plus(d: Direction) = this + d.delta
 operator fun Location.plus(delta: Delta) = first + delta.first to second + delta.second
 operator fun Location.minus(what: Location) = Direction.values().single { this == what + it }
-
 
 
 fun mapShip(memory: Memory): Map<Location, MapContent> = runBlocking {
@@ -97,15 +120,8 @@ fun mapShip(memory: Memory): Map<Location, MapContent> = runBlocking {
         .also { output(it) }
 }
 
-fun nextUnknown(location: Location, map: Map<Location, MapContent>) = when {
-    !map.containsKey(location + Direction.N) -> Direction.N
-    !map.containsKey(location + Direction.S) -> Direction.S
-    !map.containsKey(location + Direction.W) -> Direction.W
-    !map.containsKey(location + Direction.E) -> Direction.E
-    else -> null
-}
-//fun nextUnknown(location: Location, map: Map<Location, MapContent>) =
-//    Direction.values().singleOrNull { !map.containsKey(location + it) }
+fun nextUnknown(location: Location, map: Map<Location, MapContent>) =
+    Direction.values().firstOrNull { map[location + it] == null }
 
 fun output(map: Map<Location, MapContent>) {
     val minY = map.keys.map { (y, x) -> y }.min() ?: error("no data")
