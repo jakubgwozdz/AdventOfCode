776d8b0c7e53ddefa9bf2036f852694bbe891240
Jakub Gwozdz <jakub.gwozdz@gmail.com>
2019-12-15 18:05:43 +0100

    day 15 build maze

--- src/advent2019/day15/Day15.kt
+++ src/advent2019/day15/Day15.kt
@@ -1,5 +1,6 @@
 package advent2019.day15
 
+import advent2019.day15.MapContent.values
 import advent2019.intcode.Computer
 import advent2019.intcode.Memory
 import advent2019.intcode.parse
@@ -10,6 +11,7 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.math.BigInteger
+import java.math.BigInteger.ZERO
 
 fun main() {
 
@@ -26,76 +28,88 @@ fun part1(program: String): Int {
     val memory = parse(program)
 
     val map = mapShip(memory)
-    output(map)
     val start = 0 to 0
-    val end = map.filterValues { it == 2 }.keys.single()
+    val end = map.filterValues { it == MapContent.Target }.keys.single()
 
     logWithTime("$start - $end")
 
     return 0
 }
 
-fun mapShip(memory: Memory): Map<Pair<Int, Int>, Int> = runBlocking {
+typealias Location = Pair<Int, Int>
+typealias Delta = Pair<Int, Int>
+
+enum class Direction(val code: BigInteger, val delta: Delta) {
+    N(1.toBigInteger(), -1 to 0),
+    S(2.toBigInteger(), 1 to 0),
+    W(3.toBigInteger(), 0 to -1),
+    E(4.toBigInteger(), 0 to 1)
+}
+
+operator fun Location.plus(d: Direction) = this + d.delta
+operator fun Location.plus(delta: Delta) = first + delta.first to second + delta.second
+operator fun Location.minus(what: Location) = Direction.values().single { this == what + it }
+
+
+enum class MapContent(val code: BigInteger) {
+    Wall(0.toBigInteger()), Space(1.toBigInteger()), Target(2.toBigInteger());
+
+    companion object {
+        fun fromCode(c: BigInteger): MapContent {
+            return values().single { it.code == c }
+        }
+    }
+}
+
+
+fun mapShip(memory: Memory): Map<Location, MapContent> = runBlocking {
     var location = 0 to 0
-    var direction = 1
-    val map = mutableMapOf(location to direction)
+    val map = mutableMapOf(location to MapContent.Space)
+    val cameFrom = mutableMapOf<Location, Location>()
     val inBuffer = Channel<BigInteger>()
     val outBuffer = Channel<BigInteger>()
-    var moves = 0
-//        val inBuffer = ChannelInBuffer("ROBOT", Channel())
-//        val outBuffer = ChannelOutBuffer("ROBOT", Channel())
-    val robot = Computer("ROBOT", memory, inBuffer, outBuffer)
-    launch { robot.run() }
-    var hitWall = false
-    var found = false
-    while (!found) {
-//            println()
-        val nextLoc = nextLocation(direction, location)
-        inBuffer.send(direction.toBigInteger())
-        val result = outBuffer.receive().toInt()
-        map[nextLoc] = result
-        if (result == 0) {
-            // WALL
-            hitWall = true
-            direction = turnRight(direction)
-        } else {
-            if (hitWall) {
-                location = nextLoc
-                moves++
-                direction = turnLeft(direction)
+    val job = launch { Computer("ROBOT", memory, inBuffer, outBuffer).run() }
+    while (true) {
+        var direction = nextUnknown(location, map)
+        if (direction == null) { // no more moves, go back
+            val from = cameFrom[location]
+            if (from == null) {
+                inBuffer.send(ZERO)
+                break
             }
+            direction = from - location
         }
-        found = result == 2
+
+        inBuffer.send(direction.code)
+
+        val nextLoc = location + direction
+        val result = MapContent.fromCode(outBuffer.receive())
+        map[nextLoc] = result
+
+        if (result != MapContent.Wall) {
+            if (nextLoc != 0 to 0) cameFrom.computeIfAbsent(nextLoc) { location }
+            location = nextLoc
+        }
+//        output(map)
+//        logWithTime("loc = $location ; dir = $direction ; mapSize = ${map.size}")
+
     }
     coroutineContext.cancelChildren()
     map.toMap()
+        .also { output(it) }
 }
 
-fun nextLocation(direction: Int, location: Pair<Int, Int>): Pair<Int, Int> = when (direction) {
-    1 -> location.first - 1 to location.second
-    2 -> location.first + 1 to location.second
-    3 -> location.first to location.second - 1
-    4 -> location.first to location.second + 1
-    else -> error("$direction")
+fun nextUnknown(location: Location, map: Map<Location, MapContent>) = when {
+    !map.containsKey(location + Direction.N) -> Direction.N
+    !map.containsKey(location + Direction.S) -> Direction.S
+    !map.containsKey(location + Direction.W) -> Direction.W
+    !map.containsKey(location + Direction.E) -> Direction.E
+    else -> null
 }
+//fun nextUnknown(location: Location, map: Map<Location, MapContent>) =
+//    Direction.values().singleOrNull { !map.containsKey(location + it) }
 
-fun turnLeft(direction: Int): Int = when (direction) {
-    1 -> 3
-    2 -> 4
-    3 -> 2
-    4 -> 1
-    else -> error("$direction")
-}
-
-fun turnRight(direction: Int): Int = when (direction) {
-    1 -> 4
-    2 -> 3
-    3 -> 1
-    4 -> 2
-    else -> error("$direction")
-}
-
-fun output(map: Map<Pair<Int, Int>, Int>) {
+fun output(map: Map<Location, MapContent>) {
     val minY = map.keys.map { (y, x) -> y }.min() ?: error("no data")
     val maxY = map.keys.map { (y, x) -> y }.max() ?: error("no data")
     val minX = map.keys.map { (y, x) -> x }.min() ?: error("no data")
@@ -103,11 +117,11 @@ fun output(map: Map<Pair<Int, Int>, Int>) {
     (minY..maxY).map { y ->
         (minX..maxX).map { x ->
             when {
-                map[y to x] == 0 -> '#'
-                map[y to x] == 2 -> 'X'
+                map[y to x] == MapContent.Wall -> '#'
+                map[y to x] == MapContent.Target -> 'X'
                 map.keys.last() == y to x -> 'D'
-                y == 0 && x==0 -> '0'
-                map[y to x] == 1 -> '.'
+                y == 0 && x == 0 -> '0'
+                map[y to x] == MapContent.Space -> '.'
                 else -> ' '
             }
         }.joinToString("")
