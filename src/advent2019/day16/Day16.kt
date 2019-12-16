2a017b37c7e9b00744da601fecc36f72086d1449
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2019-12-16 10:50:40 +0100

    day 16 part 2 not yet working

--- src/advent2019/day16/Day16.kt
+++ src/advent2019/day16/Day16.kt
@@ -5,38 +5,77 @@ import advent2019.readAllLines
 
 fun main() {
     val input = readAllLines("input-2019-16.txt").single()
-        .also { logWithTime("input length: ${it.length}")}
-    val output = fft(input, 100).take(8)
-        .also { logWithTime("part 1: $it")}
+        .also { logWithTime("input length: ${it.length}") }
+    fft(input, 100).take(8)
+        .also { logWithTime("part 1: $it") }
+
+    fftRepeat(input, 10000, 100, 7, 8)
+        .also { logWithTime("part 2: $it") }
 
 }
 
-fun fftPattern(i: Int, l: Int): List<Int> {
-    val p = listOf(0, 1, 0, -1)
-    var t = 0
-    return generateSequence { p[t++ % 4] }
-        .flatMap { f -> sequence { repeat(i) { yield(f) } } }
-        .drop(1)
-        .take(l)
-        .toList()
+// indices are 1-based
+fun fftPatternSingle(k: Int, i: Int): Int {
+    return when ((k / i) % 4) {
+        0 -> 0
+        1 -> 1
+        2 -> 0
+        3 -> -1
+        else -> error("$k, $i")
+    }
+}
+
+interface DigitsProvider {
+    // 1-based
+    operator fun get(i: Int): Int
+
+    val size: Int
+}
+
+class StringDigitsProvider(val input: String, repeats: Int = 1) : DigitsProvider {
+    override fun get(i: Int) = input[(i - 1) % input.length] - '0'
+    override val size: Int = input.length * repeats
+}
+
+class CachingDigitsProvider(val input: DigitsProvider, val op: (Int) -> Int) : DigitsProvider {
+    val cache = mutableMapOf<Int, Int>()
+    override fun get(i: Int) = cache.computeIfAbsent(i) { op(i) }
+    override val size: Int = input.size
+}
+
+fun fftSingle(input: DigitsProvider, i: Int): Int {
+    val sum = (1..input.size).map { it to input[it] }
+        .map { (k, d) -> d * fftPatternSingle(k, i) }
+        .sum()
+    return when {
+        sum > 0 -> sum % 10
+        sum < 0 -> (-sum) % 10
+        else -> 0
+    }
+}
+
+fun fft(input: String, iterations: Int): String {
+    return fft(StringDigitsProvider(input), iterations)
+        .let { provider -> buildString { repeat(provider.size) { append(provider[it + 1]) } } }
+}
+
+fun fft(input: DigitsProvider, iterations: Int): DigitsProvider {
+    return (1..iterations).fold(input) { i, _ -> fftPhase(i) }
+}
+
+fun fftPhase(input: DigitsProvider): DigitsProvider {
+    return CachingDigitsProvider(input) { fftSingle(input, it) }
 }
 
 fun fftPhase(input: String): String {
-    return input.mapIndexed { i, c ->
-        val fftPattern = fftPattern(i + 1, input.length)
-        val sum = input.map { "$it".toInt() }
-            .zip(fftPattern)
-            .map { (c, p) -> c * p }
-            .sum()
-        when {
-            sum > 0 -> sum % 10
-            sum < 0 -> (-sum) % 10
-            else -> 0
-        }
-    }.joinToString("")
+    return fftPhase(StringDigitsProvider(input))
+        .let { provider -> buildString { repeat(provider.size) { append(provider[it + 1]) } } }
 }
 
-fun fft(input: String, iterations: Int) :String {
-    return (1..iterations).fold(input) { i,_-> fftPhase(i)}
+fun fftRepeat(input: String, repeats: Int, iterations: Int, offset: Int, messageLen: Int): String {
+    return fft(
+        StringDigitsProvider(input, repeats),
+        iterations
+    )
+        .let { provider -> buildString { repeat(messageLen) { append(provider[it + 1 + offset]) } } }
 }
-
--- testSrc/advent2019/day16/Day16KtTest.kt
+++ testSrc/advent2019/day16/Day16KtTest.kt
@@ -1,6 +1,9 @@
 package advent2019.day16
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import java.time.Duration
 import kotlin.test.expect
 
 internal class Day16KtTest {
@@ -9,6 +12,11 @@ internal class Day16KtTest {
     internal fun testPattern() {
         expect(listOf(1, 0, -1, 0, 1, 0, -1, 0)) { fftPattern(1, 8) }
         expect(listOf(0, 1, 1, 0, 0, -1, -1, 0)) { fftPattern(2, 8) }
+        expect(listOf(0, 0, 1, 1, 1, 0, 0, 0)) { fftPattern(3, 8) }
+    }
+
+    private fun fftPattern(i: Int, l: Int): List<Int> {
+        return (1..l).map { fftPatternSingle(it, i) }
     }
 
     @Test
@@ -23,8 +31,39 @@ internal class Day16KtTest {
     internal fun testFft() {
         expect("48226158") { fft("12345678", 1) }
         expect("01029498") { fft("12345678", 4) }
+    }
+
+    @Test
+    internal fun testPart1() {
         expect("24176176") { fft("80871224585914546619083218645595", 100).take(8) }
         expect("73745418") { fft("19617804207202209144916044189917", 100).take(8) }
         expect("52432133") { fft("69317163492948606335995924319873", 100).take(8) }
     }
+
+    @Test
+    internal fun testPart2a() {
+        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(3)) {
+            expect("24176176") { fftRepeat("80871224585914546619083218645595", 1, 100, 0, 8) }
+        }
+        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(3)) {
+            expect("73745418") { fftRepeat("19617804207202209144916044189917", 1, 100, 0, 8) }
+        }
+        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(3)) {
+            expect("52432133") { fftRepeat("69317163492948606335995924319873", 1, 100, 0, 8) }
+        }
+    }
+
+    @Test
+    internal fun testPart2b() {
+        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(3)) {
+            expect("84462026") { fftRepeat("03036732577212944063491565474664", 10000, 100, 303673, 8) }
+        }
+        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(3)) {
+            expect("78725270") { fftRepeat("02935109699940807407585447034323", 10000, 100, 293510, 8) }
+        }
+        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(3)) {
+            expect("53553731") { fftRepeat("03081770884921959731165446850517", 10000, 100, 308177, 8) }
+        }
+    }
+
 }
