ca62bf6c1fa6af9d4325e17c3fc172273061a7e6
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2019-12-16 08:47:47 +0100

    day 16 part 1

--- src/advent2019/day16/Day16.kt
+++ src/advent2019/day16/Day16.kt
@@ -1,7 +1,14 @@
 package advent2019.day16
 
-fun fftPhase(input: String): String {
-    TODO()
+import advent2019.logWithTime
+import advent2019.readAllLines
+
+fun main() {
+    val input = readAllLines("input-2019-16.txt").single()
+        .also { logWithTime("input length: ${it.length}")}
+    val output = fft(input, 100).take(8)
+        .also { logWithTime("part 1: $it")}
+
 }
 
 fun fftPattern(i: Int, l: Int): List<Int> {
@@ -12,5 +19,24 @@ fun fftPattern(i: Int, l: Int): List<Int> {
         .drop(1)
         .take(l)
         .toList()
+}
+
+fun fftPhase(input: String): String {
+    return input.mapIndexed { i, c ->
+        val fftPattern = fftPattern(i + 1, input.length)
+        val sum = input.map { "$it".toInt() }
+            .zip(fftPattern)
+            .map { (c, p) -> c * p }
+            .sum()
+        when {
+            sum > 0 -> sum % 10
+            sum < 0 -> (-sum) % 10
+            else -> 0
+        }
+    }.joinToString("")
+}
+
+fun fft(input: String, iterations: Int) :String {
+    return (1..iterations).fold(input) { i,_-> fftPhase(i)}
 }
 
--- testSrc/advent2019/day16/Day16KtTest.kt
+++ testSrc/advent2019/day16/Day16KtTest.kt
@@ -4,6 +4,13 @@ import org.junit.jupiter.api.Test
 import kotlin.test.expect
 
 internal class Day16KtTest {
+
+    @Test
+    internal fun testPattern() {
+        expect(listOf(1, 0, -1, 0, 1, 0, -1, 0)) { fftPattern(1, 8) }
+        expect(listOf(0, 1, 1, 0, 0, -1, -1, 0)) { fftPattern(2, 8) }
+    }
+
     @Test
     internal fun testPhase() {
         expect("48226158") { fftPhase("12345678") }
@@ -13,8 +20,11 @@ internal class Day16KtTest {
     }
 
     @Test
-    internal fun testPattern() {
-        expect(listOf(1, 0, -1, 0, 1, 0, -1, 0)) { fftPattern(1, 8) }
-        expect(listOf(0, 1, 1, 0, 0, -1, -1, 0)) { fftPattern(2, 8) }
+    internal fun testFft() {
+        expect("48226158") { fft("12345678", 1) }
+        expect("01029498") { fft("12345678", 4) }
+        expect("24176176") { fft("80871224585914546619083218645595", 100).take(8) }
+        expect("73745418") { fft("19617804207202209144916044189917", 100).take(8) }
+        expect("52432133") { fft("69317163492948606335995924319873", 100).take(8) }
     }
 }
