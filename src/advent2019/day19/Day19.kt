1ed55acf819df79c32970b2336ea65c58359e350
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2019-12-19 11:17:52 +0100

    Day 19 p2 assume y-progression > x-progression

--- src/advent2019/day19/Day19.kt
+++ src/advent2019/day19/Day19.kt
@@ -14,40 +14,68 @@ fun main() {
     val input = readAllLines("input-2019-19.txt").single()
         .also { logWithTime("Program length (chars): ${it.length}") }
 
-    var last = -1 to (0 until 0)
+    var pivot = -1 to (0 until 0)
 
-    var scan = 0..0
+    var nextScan = 0..0
     (0 until 50).map { y ->
-        val lastResult = last.second
-        repeat(scan.first) { print(" ") }
+        val lastResult = pivot.second
+        repeat(nextScan.first) { print(" ") }
         val result = if (lastResult.count() < 2) {
-            val newLine = scan.map { x ->
-                BigInteger.ONE == call(input, x, y)
-                    .also { print(it) }
+            val newLine = nextScan.map { x ->
+                call(input, x, y)
+                    .also { print(if (it) "#" else ".") }
             }
-            if (newLine.none { it }) scan.last + 1..scan.last
-            else scan.first + newLine.indexOfFirst { it }..scan.first + newLine.indexOfLast { it }
+            if (newLine.none { it }) nextScan.last + 1..nextScan.last
+            else nextScan.first + newLine.indexOfFirst { it }..nextScan.first + newLine.indexOfLast { it }
         } else {
-            val f0 = call(input, lastResult.first, y)
-                .also { print(it) }
-            val newStart = if (f0 == BigInteger.ONE) lastResult.first else lastResult.first + 1
-            val l1 = call(input, lastResult.last + 1, y)
-                .also { repeat(lastResult.count() - 1) { print(" ") } }
-                .also { print(it) }
-            val newEnd = if (l1 != BigInteger.ONE) lastResult.last else lastResult.last + 1
+            var newStart = lastResult.first
+            while (!call(input, newStart, y).also { print(if (it) "#" else ".") }) newStart++
+            var newEnd = lastResult.last.coerceAtLeast(newStart)
+            repeat(newEnd - newStart) { print(" ") }
+            while (call(input, newEnd + 1, y).also { print(if (it) "#" else ".") }) newEnd++
             (newStart..newEnd)
         }
 
-        scan = (if (result.isEmpty()) scan.first else result.first)..result.last + 1
-        last = y to result
+        nextScan = (if (result.isEmpty()) nextScan.first else result.first)..result.last + 1
+        pivot = (y to result).also { println(" ; pivot = $it") }
         result
-            .also { println(" ; result = $result") }
     }.sumBy { it.count() }
         .also { logWithTime("part1: $it") }
 
+    val w = 100
+    val h = 100
+
+    var y = (pivot.first * w + pivot.second.first * h) / pivot.second.count()
+    var matching = 0 to 0
+
+    do {
+        var t = findFor(y, pivot, input)
+        pivot = y to t
+        var b = findFor(y + h - 1, pivot, input)
+        pivot = y + h - 1 to b
+        val ww = t.last + 1 - b.first
+        println("$y: $t - $b ($ww)")
+        matching = b.first to y++
+    } while (ww < w)
+
+    logWithTime("part2: ${matching.first * 10000 + matching.second}")
+
 }
 
-fun call(program: String, x: Int, y: Int): BigInteger =
+fun findFor(y: Int, pivot: Pair<Int, IntRange>, input: String): IntRange {
+    val (py, pr) = pivot
+    var first = pr.first * y / py
+    var last = pr.last * y / py
+    while (!call(input, first, y)) first++
+    while (call(input, first - 1, y)) first--
+
+    while (!call(input, last, y)) last--
+    while (call(input, last + 1, y)) last++
+    return first..last
+}
+
+
+fun call(program: String, x: Int, y: Int): Boolean =
     runBlocking {
 
         val inChannel = Channel<BigInteger>()
@@ -61,4 +89,5 @@ fun call(program: String, x: Int, y: Int): BigInteger =
         inChannel.send(x.toBigInteger())
         inChannel.send(y.toBigInteger())
         outChannel.receive()
-    }
+//            .also { print("$x,$y=$it;") }
+    } == BigInteger.ONE
