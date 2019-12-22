78a1fc53fdc25ce0728f0aca7a678c27a37ad7ed
Jakub Gwozdz <jakub.gwozdz@gmail.com>
2019-12-22 19:35:02 +0100

    day 22 part 1

--- src/advent2019/day22/Day22.kt
+++ src/advent2019/day22/Day22.kt
@@ -1,1 +1,49 @@
 package advent2019.day22
+
+import advent2019.logWithTime
+import advent2019.readAllLines
+
+val dealIncRegex = Regex("deal with increment (-?\\d+)")
+val newStackRegex = Regex("deal into new stack")
+val cutRegex = Regex("cut (-?\\d+)")
+
+
+fun deal(deckSize: Int, input: List<String>, times: Int = 1): List<Int> {
+
+    val deck = IntArray(deckSize) { it }
+
+    repeat(times) {
+        input.forEach {
+            dealIncRegex.matchEntire(it)?.destructured?.run { dealInc(deck, component1().toInt()) }
+                ?: newStackRegex.matchEntire(it)?.destructured?.run { reverse(deck) }
+                ?: cutRegex.matchEntire(it)?.destructured?.run { cut(deck, component1().toInt()) }
+                ?: error("'$it' is not proper")
+        }
+    }
+    return deck.toList()
+
+}
+
+fun reverse(deck: IntArray) {
+    val table = IntArray(deck.size) { deck[deck.size - it - 1] }
+    table.copyInto(deck)
+}
+
+fun cut(deck: IntArray, i: Int) {
+    val j = if (i < 0) deck.size + i else i
+    val table = IntArray(deck.size) { deck[(it + j) % deck.size] }
+    table.copyInto(deck)
+}
+
+fun dealInc(deck: IntArray, increment: Int) {
+    val table = IntArray(deck.size) { deck[it] }
+    deck.indices.forEach { deck[it * increment % deck.size] = table[it] }
+}
+
+fun main() {
+
+    val input = readAllLines("input-2019-22.txt")
+
+    deal(10007, input).indexOfFirst { it == 2019 }
+        .also { logWithTime("part 1: $it") }
+}
--- testSrc/advent2019/day22/Day22KtTest.kt
+++ testSrc/advent2019/day22/Day22KtTest.kt
@@ -1,2 +1,78 @@
 package advent2019.day22
-internal class Day22KtTest { }
+
+import org.junit.jupiter.api.Test
+import kotlin.test.expect
+
+internal class Day22KtTest {
+
+    val input1 = """deal with increment 7
+deal into new stack
+deal into new stack""".lines()
+
+    @Test
+    fun test1() {
+        expect("0 3 6 9 2 5 8 1 4 7") { deal(10, input1).joinToString(" ") }
+    }
+
+    val input2 = """cut 6
+deal with increment 7
+deal into new stack""".lines()
+
+    @Test
+    fun test2() {
+        expect("3 0 7 4 1 8 5 2 9 6") { deal(10, input2).joinToString(" ") }
+    }
+
+    val input3 = """deal with increment 7
+deal with increment 9
+cut -2""".lines()
+
+    @Test
+    fun test3() {
+        expect("6 3 0 7 4 1 8 5 2 9") { deal(10, input3).joinToString(" ") }
+    }
+
+    val input4 = """deal into new stack
+cut -2
+deal with increment 7
+cut 8
+cut -4
+deal with increment 7
+cut 3
+deal with increment 9
+deal with increment 3
+cut -1""".lines()
+
+    val input4a = input4.drop(1)
+
+    @Test
+    fun test4() {
+        expect("9 2 5 8 1 4 7 0 3 6") { deal(10, input4).joinToString(" ") }
+    }
+
+    @Test
+    fun repetitions() {
+
+        (10..100).filter { it % 3 != 0 && it % 7 != 0 }.forEach { deckSize ->
+            try {
+                val cycle1 = cycle(input1, deckSize)
+                val cycle2 = cycle(input2, deckSize)
+                val cycle3 = cycle(input3, deckSize)
+                val cycle4 = cycle(input4, deckSize)
+                val cycle4a = cycle(input4a, deckSize)
+                println("$deckSize: $cycle1 $cycle2 $cycle3 $cycle4 $cycle4a")
+            } catch (e: Exception) {
+                println("$deckSize: $e")
+            }
+        }
+
+    }
+
+    fun cycle(input: List<String>, deckSize: Int): Int {
+        return (1..deckSize).map { it to deal(deckSize, input, it) }
+            .first { it.second == deal(deckSize, input, 0) }
+            .first
+    }
+
+
+}
