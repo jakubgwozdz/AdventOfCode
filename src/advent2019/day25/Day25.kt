eaa233c4472d57988051843e50753c65910a8b3e
JG - surf <jakub.gwozdz@gmail.com>
2019-12-29 12:54:00 +0000

    day 25 goto security

--- src/advent2019/day25/Day25.kt
+++ src/advent2019/day25/Day25.kt
@@ -152,9 +152,22 @@ class SearchUpdater(val state: SearchState) {
         if (prevRoomId != null && state.lastMovement != null) {
             state.knownExits.computeIfAbsent(prevRoomId) { mutableMapOf() }[state.lastMovement!!] = room.name
         }
-        state.knownDirectionsToPlaces.computeIfAbsent(room.name) { state.movements.toList() }
+        state.knownDirectionsToPlaces.computeIfAbsent(room.name) { state.movements.toList().compact { it.first } }
     }
+}
 
+private fun <E, T> List<E>.compact(discriminatorOp: (E) -> T): List<E> {
+    val result = this.toList()
+
+    val loops = this.groupBy(discriminatorOp)
+        .mapValues { (_, v) -> v.count() }
+        .filterValues { it> 1 }
+        .keys
+    val i = indexOfFirst { discriminatorOp(it) in loops }
+    if (i < 0) return result
+
+    val j = indexOfLast { discriminatorOp(it) == discriminatorOp(this[i]) }
+    return (result.subList(0, i) + result.subList(j, this.size)).compact(discriminatorOp)
 }
 
 @FlowPreview
