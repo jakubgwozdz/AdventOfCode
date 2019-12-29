aab9592c50c64c3a61e9b3a5178a74f758281786
JG - surf <jakub.gwozdz@gmail.com>
2019-12-29 21:06:47 +0000

    some tests

--- testSrc/advent2019/day13/Day13KtTest.kt
+++ testSrc/advent2019/day13/Day13KtTest.kt
@@ -1,3 +1,20 @@
 package advent2019.day13
 
-internal class Day13KtTest
+import advent2019.readAllLines
+import org.junit.jupiter.api.Test
+import kotlin.test.expect
+
+internal class Day13KtTest {
+    @Test
+    fun puzzlePart1() {
+        val input = readAllLines("data/input-2019-13.txt").single()
+        expect(265) { part1(input) }
+    }
+
+    @Test
+    fun puzzlePart2() {
+        val input = readAllLines("data/input-2019-13.txt").single()
+        expect(13331) { part2(input) }
+    }
+
+}
