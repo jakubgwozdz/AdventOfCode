8f812a0123819939c110092017eebebb5ffea92d
JG - surf <jakub.gwozdz@gmail.com>
2019-12-29 14:15:26 +0000

    day 25 PART 1 TEST

--- testSrc/advent2019/day25/Day25KtTest.kt
+++ testSrc/advent2019/day25/Day25KtTest.kt
@@ -1,3 +1,17 @@
 package advent2019.day25
 
-internal class Day25KtTest
+import advent2019.intcode.parseIntcode
+import advent2019.readAllLines
+import kotlinx.coroutines.FlowPreview
+import org.junit.jupiter.api.Test
+import kotlin.test.expect
+
+private val input = readAllLines("data/input-2019-25.txt").single()
+
+@FlowPreview
+internal class Day25KtTest {
+    @Test
+    fun puzzlePart1() {
+        expect("84410376") { Cryostasis(parseIntcode(input)).start() }
+    }
+}
