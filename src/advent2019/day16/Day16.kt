067426044fcf4e40ffd3ccbb53f7a1c57fff7ae2
Jakub Gwozdz <jakub.gwozdz@gmail.com>
2020-01-02 21:37:40 +0100

    day 16 part 2

--- src/advent2019/day16/Day16.kt
+++ src/advent2019/day16/Day16.kt
@@ -17,9 +17,9 @@ fun main() {
 
 fun fftSingleDigit(input: IntArray, i: Int): Int {
     var sum = 0
-    (i+1 .. input.size).forEach {
-        if ((it / (i + 1)) % 4 == 1) sum += input[it-1]
-        if ((it / (i + 1)) % 4 == 3) sum -= input[it-1]
+    (i + 1..input.size).forEach {
+        if ((it / (i + 1)) % 4 == 1) sum += input[it - 1]
+        if ((it / (i + 1)) % 4 == 3) sum -= input[it - 1]
     }
 
     return when {
@@ -33,16 +33,27 @@ fun fftPhase(input: IntArray): IntArray {
     return IntArray(input.size) { fftSingleDigit(input, it) }
 }
 
-fun fftRepeat(input: String, repeats: Int, iterations: Int, offset: Int, messageLen: Int): String {
-    TODO()
-}
-
 fun fft(input: String, iterations: Int, offset: Int = 0, messageLen: Int = input.length): String {
     val fft = (1..iterations).fold(input.toDigits()) { provider, iter ->
         fftPhase(provider)
     }
     return fft.asString(offset, messageLen)
 }
+
+fun fftRepeat(input: String, repeats: Int, iterations: Int, offset: Int, messageLen: Int): String {
+    if (offset < input.length * repeats / 2) error("shortcut won't work")
+    var secondHalf = (offset until input.length * repeats).map { it to (input[it % input.length] - '0') }.toMap()
+    repeat(iterations) {
+        var a = 0
+        secondHalf = (input.length * repeats - 1 downTo offset).map { i ->
+            i to (a + secondHalf[i]!!)
+                .also { a = it }
+                .let { if (it < 0) -it else it } % 10
+        }.toMap()
+    }
+    return (offset until offset + messageLen).map { secondHalf[it]!! }.joinToString("")
+}
+
 
 internal fun String.toDigits() = IntArray(length) { this[it] - '0' }
 internal fun IntArray.asString(offset: Int = 0, messageLen: Int = this.size) =
--- testSrc/advent2019/day16/Day16KtTest.kt
+++ testSrc/advent2019/day16/Day16KtTest.kt
@@ -35,20 +35,6 @@ internal class Day16KtTest {
     }
 
     @Test
-    fun testPart2a() {
-        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(10)) {
-            expect("24176176") { fftRepeat("80871224585914546619083218645595", 1, 100, 0, 8) }
-        }
-        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(3)) {
-            expect("73745418") { fftRepeat("19617804207202209144916044189917", 1, 100, 0, 8) }
-        }
-        Assertions.assertTimeoutPreemptively(Duration.ofSeconds(3)) {
-            expect("52432133") { fftRepeat("69317163492948606335995924319873", 1, 100, 0, 8) }
-        }
-    }
-
-    @Test
-    @Disabled
     fun testPart2b() {
         Assertions.assertTimeoutPreemptively(Duration.ofSeconds(10)) {
             expect("84462026") { fftRepeat("03036732577212944063491565474664", 10000, 100, 303673, 8) }
@@ -63,34 +49,34 @@ internal class Day16KtTest {
 
     @Test
     fun testRepeated() {
-        expect("34389886592132718868384101444361") {
-            fftRepeat("69317163492948616931716349294861", 1, 10, 0, 32)
+        expect("4361") {
+            fft("69317163492948616931716349294861", 10, 28, 4)
         }
-        expect("34389886592132718868384101444361") {
-            fftRepeat("6931716349294861", 2, 10, 0, 32)
+        expect("4361") {
+            fftRepeat("6931716349294861", 2, 10, 28, 4)
         }
     }
 
     @Test
     fun testRepeated2() {
-        expect("542132465648252") {
-            fftRepeat("432524325243252", 1, 10, 0, 15)
+        expect("48252") {
+            fft("432524325243252",  10, 10, 5)
         }
-        expect("542132465648252") {
-            fftRepeat("43252", 3, 10, 0, 15)
+        expect("48252") {
+            fftRepeat("43252", 3, 10, 10, 5)
         }
     }
 
     @Test
     fun testRepeated3a() {
-        expect("403893683670591085908457663043256980782088917468785168225711620724036") {
-            fftRepeat("432528237465328745290364325282374653287452903643252823746532874529036", 1, 10, 0, 69)
+        expect("620724036") {
+            fft("432528237465328745290364325282374653287452903643252823746532874529036", 10, 60, 9)
         }
     }
     @Test
     fun testRepeated3b() {
-        expect("403893683670591085908457663043256980782088917468785168225711620724036") {
-            fftRepeat("43252823746532874529036", 3, 10, 0, 69)
+        expect("620724036") {
+            fftRepeat("43252823746532874529036", 3, 10, 60, 9)
         }
     }
 
@@ -105,7 +91,14 @@ internal class Day16KtTest {
     @Test
     fun testPart1Input() {
         expect("73127523") {
-            fft(readAllLines("data/input-2019-16.txt").single(), 100).take(8)
+            fft(readAllLines("data/input-2019-16.txt").single(), 100, 0, 8)
+        }
+    }
+
+    @Test
+    fun testPart2Input() {
+        expect("73127523") {
+            fftRepeat(readAllLines("data/input-2019-16.txt").single(), 10000, 100, 5973857, 8)
         }
     }
 
